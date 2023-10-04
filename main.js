93f407b3a92956676fbf34df91d4775189662527
Giovany Lawall Inocêncio <ino.giovany@gmail.com>
2023-10-04 07:33:04 -0300

    Refactoring and input validation

--- main.js
+++ main.js
@@ -1,31 +1,49 @@
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
-const randomNumber = Math.round(Math.random() * 10)
-let xAttempts = 1
-
-// função callback
-function handleTryClick(event) {
-  event.preventDefault()
-  const inputNumber = document.querySelector("#inputNumber")
-
-  if (Number(inputNumber.value) == randomNumber) {
-    screen1.classList.add("hide")
-    screen2.classList.remove("hide")
-
-    document.querySelector(".screen2 h2").innerText = `Acertou em ${xAttempts} tentativas`
-  }
-  inputNumber.value = ""
-  xAttempts++
-}
-
-// eventos - tirar responsabilidade de chamar a função do HTML
 
 const btnTry = document.querySelector('#btnTry')
 const btnReset = document.querySelector('#btnReset')
 
+let randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1
+
+// eventos - tirar responsabilidade de chamar a função do HTML
 btnTry.addEventListener("click", handleTryClick)
-btnReset.addEventListener("click", function () {
-  screen1.classList.remove("hide")
-  screen2.classList.add("hide")
+btnReset.addEventListener("click", handleResetClick)
+document.addEventListener('keydown', handleEnterReset)
+
+// funcções callback
+function handleTryClick(event) {
+  const inputNumber = document.querySelector("#inputNumber")
+  event.preventDefault()
+  if (inputNumber.value == "" || inputNumber.value < 0 || inputNumber.value > 10) {
+    document.querySelector('.msgEmptyInput').classList.remove("hide")
+    document.querySelector(".msgTryAgain").classList.add("hide")
+    inputNumber.value = ""
+  } else {
+    if (Number(inputNumber.value) == randomNumber) {
+      toggleScreen()
+      screen2.querySelector("h2").innerText = `Acertou em ${xAttempts} tentativas`
+    }
+    inputNumber.value = ""
+    document.querySelector(".msgTryAgain").classList.remove("hide")
+    document.querySelector('.msgEmptyInput').classList.add("hide")
+    xAttempts++
+  }
+}
+function handleResetClick() {
+  toggleScreen()
   xAttempts = 1
-})
+  document.querySelector(".msgTryAgain").classList.add("hide")
+  randomNumber = Math.round(Math.random() * 10)
+}
+function handleEnterReset(event) {
+  if (event.key === 'Enter' && screen1.classList.contains('hide')) {
+    handleResetClick()
+  }
+}
+
+function toggleScreen() {
+  screen1.classList.toggle("hide")
+  screen2.classList.toggle("hide")
+}
